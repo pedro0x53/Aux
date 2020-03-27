ac0a00cd1d404876e858d5bde79d9ea3bb886bf0
pedrosousa27 <pedrosousa2708@gmail.com>
2020-03-27 17:01:06 -0300

    Base do projeto

---
+++ Aux/core.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public class Core {
+    
+    public func checkFile() -> Bool {
+        var res: Bool = false
+        return res
+    }
+    
+    public func createFile() {}
+    public func readFile() {}
+    public func deleteFile() {}
+    
+    public func listApps() {}
+    public func openApp() {}
+    
+}
