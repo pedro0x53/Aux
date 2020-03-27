505221eb13e75a95af9ea6e59c14acadf56b494a
pedrosousa27 <pedrosousa2708@gmail.com>
2020-03-27 16:19:01 -0300

    Initial Commit

---
+++ Aux/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Aux
+//
+//  Created by Pedro Sousa on 27/03/20.
+//  Copyright © 2020 Pedro Sousa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
