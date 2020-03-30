4dfcd7a68e700eb6554df3cfee94940948d1c63f
pedrosousa27 <pedrosousa2708@gmail.com>
2020-03-30 10:49:49 -0300

    Funções para criar diretório base e checar se existem arquivos de ambientes criados

--- Aux/core.swift
+++ Aux/core.swift
@@ -2,9 +2,35 @@ import Foundation
 
 public class Core {
     
-    public func checkFile() -> Bool {
-        var res: Bool = false
-        return res
+    var fileManager: FileManager
+    var folderURL: URL? = nil
+    
+    init() {
+        self.fileManager = FileManager.default
+        self.folderURL = checkFolder()
+    }
+    
+    public func checkFolder() -> URL? {
+        let docsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let envFolder = docsURL.appendingPathComponent("Envs")
+        do {
+            try fileManager.createDirectory(at: envFolder, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            return nil
+        }
+    
+        return envFolder
+    }
+    
+    public func checkFiles() -> [String?] {
+        guard let folderURL = checkFolder() else { return [nil] }
+        var filesPaths = Array<String?>()
+        do {
+            try filesPaths = fileManager.contentsOfDirectory(atPath: folderURL.path)
+        } catch {
+            filesPaths = [nil]
+        }
+        return filesPaths
     }
     
     public func createFile() {}
