d305e2a5c0994f372a8c6f217b93ec5fea201c0e
pedrosousa27 <pedrosousa2708@gmail.com>
2020-03-30 10:48:45 -0300

    Arquivo renomeado de main.swift para aux.swift

--- Aux/main.swift
+++ Aux/main.swift
@@ -1,34 +1,4 @@
 import Foundation
 
-class Aux {
-    let core: Core
-    
-    init() {
-        self.core = Core()
-    }
-    
-    public func input() -> String {
-        var request: String = ""
-        return request
-    }
-    
-    public func output(responses: [String]) {
-        for response in responses {
-            print(response)
-        }
-    }
-    
-    public func process(arg: String) -> (Int, String) {
-        var code: Int = 0
-        var request: String = ""
-        
-        return (code, request)
-    }
-    
-    public func execute(request: (code: Int, req: String)) {}
-    
-    public func createEnviroment(env: String) {}
-    public func listEnviroment() {}
-    public func executeEnviroment(env: String) {}
-    public func deleteEnviroment(env: String) {}
-}
+let aux = Aux()
+print()
