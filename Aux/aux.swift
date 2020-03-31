433fb1032f548829b49010973ff6f98da081838e
pedrosousa27 <pedrosousa2708@gmail.com>
2020-03-31 13:03:21 -0300

    Função input e process, sempre que um processo terminar de ser executado, a função input inicia um novo loop até que o comando "exit" seja executado

--- Aux/aux.swift
+++ Aux/aux.swift
@@ -7,28 +7,48 @@ class Aux {
         self.core = Core()
     }
     
-    public func input() -> String {
-        var request: String = ""
-        return request
-    }
-    
-    public func output(responses: [String]) {
-        for response in responses {
-            print(response)
+    public func input() {
+        while let req = readLine() {
+            process(arg: req)
         }
     }
     
-    public func process(arg: String) -> (Int, String) {
-        var code: Int = 0
-        var request: String = ""
+    public func process(arg: String = "") {
+        var args = arg.split(separator: " ")
+        let cmd = args[0]
+        args.remove(at: 0)
+        let content = args.joined(separator: " ")
         
-        return (code, request)
+        switch cmd {
+        case "list":
+            listEnvironment()
+        case "create":
+            createEnvironment(env: content)
+        case "run":
+            executeEnvironment(env: content)
+        case "delete":
+            deleteEnvironment(env: content)
+        case "exit":
+            exit(0)
+        default:
+            print("Error: Command not recognized by the system.")
+        }
+        
     }
     
-    public func execute(request: (code: Int, req: String)) {}
+    public func createEnvironment(env: String) {
+        print("Creating...")
+    }
     
-    public func createEnviroment(env: String) {}
-    public func listEnviroment() {}
-    public func executeEnviroment(env: String) {}
-    public func deleteEnviroment(env: String) {}
+    public func listEnvironment() {
+        print("Listing...")
+    }
+    
+    public func executeEnvironment(env: String) {
+            print("Running...")
+    }
+    
+    public func deleteEnvironment(env: String) {
+        print("Deleting...")
+    }
 }
